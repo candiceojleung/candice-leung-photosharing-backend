27e619c3d1920b1a40ea6d99a3562c104a0dc816
candiceojleung <candice.leung@live.ca>
2025-02-25 00:22:52 -0500

    create route to post comments

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 const app = express();
 const port = process.env.PORT || process.argv[2] || 8080;
-import { v4 as uuidv4 } from "uuid";
 import cors from "cors";
 import "dotenv/config";
 import tags from "./routes/tags.js";
@@ -18,17 +17,8 @@ app.get('/photos', photos)
 
 app.get('/photos/:id', photos)
 
-app.post("/photos/:id/comments", (req, res) => {
-    const { name,comment } = req.body;
-    const newComment = {
-      id:uuidv4(),
-      name,
-      comment,
-      timestamp: Date.now(),
-    };
-    comments.push(newComment);
-    res.json(newComment);
-  });
-  
+app.get('/photos/:id/comments',photos)
 
+app.post('/photos/:id/comments',photos); 
+  
 app.listen(port, () => console.log(`Listening on ${port}`));
--- routes/photos.js
+++ routes/photos.js
@@ -1,6 +1,8 @@
 import express from "express";
 const router = express.Router();
 import fs from "fs";
+import { v4 as uuidv4 } from "uuid";
+
 
 function readPhotos() {
     const photosData = fs.readFileSync("./data/photos.json");
@@ -29,6 +31,20 @@ router.get("/photos/:id/comments", (req,res)=>{
     const photo = getPhotoById(id);
     res.json(photo.comments)
 })
+
+router.post("/photos/:id/comments", (req, res) => {
+    const id = req.params.id;
+    const photo = getPhotoById(id);
+    const { name, comment } = req.body;
+    const newComment = {
+      id: uuidv4(),
+      name,
+      comment,
+      timestamp: Date.now()
+    };
+    (photo.comments).push(newComment);
+    res.json(newComment);
+  });
 
 
 export default router
