09031c362dbd5a1a2fc488411bf0cfc29e444f89
candiceojleung <candice.leung@live.ca>
2025-02-24 21:08:16 -0500

    set up tags api

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
 const app = express();
 const port = process.env.PORT || process.argv[2] || 8080;
-import { v4 as uuidv4 } from "uuid";
-app.use(express.json());
+// import { v4 as uuidv4 } from "uuid";
 import cors from "cors";
 import "dotenv/config";
+import tags from "./routes/tags.js";
+
+app.use(express.json());
 
 const { CORS_ORIGIN } = process.env;
 app.use(cors({ origin: CORS_ORIGIN }));
+
+app.get ('/tags', tags)
 
 app.listen(port, () => console.log(`Listening on ${port}`));
---
+++ routes/tags.js
@@ -0,0 +1,16 @@
+import express from "express";
+const router = express.Router();
+import fs from "fs";
+
+function readTags() {
+	const tagsData = fs.readFileSync("./data/tags.json");
+	const parsedTags = JSON.parse(tagsData);
+	return parsedTags;
+}
+
+router.get("/tags", (req,res)=>{
+    const tags= readTags(); 
+    res.json(tags)
+});
+
+export default router
